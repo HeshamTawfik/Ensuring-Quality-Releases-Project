6912cdb7475df7560142f074220108bc7fa07cca
Hesham Tawfik <33587812+HeshamTawfik@users.noreply.github.com>
2023-11-26 14:23:38 +0200

    Delete terraform directory

--- terraform/modules/vm/input.tf
+++
@@ -1,8 +0,0 @@
-variable "public_ip" {}
-variable "resource_group" {}
-variable "location" {}
-variable "name" {}
-variable "subnet_id" {}
-variable "admin_username" {}
-variable "packer_image" {}
-variable "public_key_path" {}
